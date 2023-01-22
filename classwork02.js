3b0c8e5a0928918a018e5bcc9fd067fecce610b6
Hanna Ivanova <hanna.ivanovadl@gmail.com>
2023-01-22 22:37:35 +0300

    if else statement ternary operator

--- classwork02.js
+++ classwork02.js
@@ -1,3 +1,13 @@
 const unreadMessagesCount = 56;
 const isUserregistered = true;
 const userFavouriteGenres = 'science fiction';
+
+const user = "Admin";
+const adminRole = "Anonymus";
+
+if (user == adminRole) {
+    console.log("Привет, Admin")
+} else {console.log("Привет, Anonymus")
+}
+
+let isAdmin = (user == adminRole) ? console.log("Привет, Admin"): console.log("Привет, Anonymus"); 
