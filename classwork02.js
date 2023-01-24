45ce8efdd10bfb9fc0211d76f33f130f466a603c
Hanna Ivanova <101669502+Hanna-ivanova@users.noreply.github.com>
2023-01-24 21:26:28 +0300

    Update classwork02.js

--- classwork02.js
+++ classwork02.js
@@ -11,4 +11,58 @@ if (currentUser === adminRole) {
 } else {console.log("Привет, Anonymus")
 }
 
-let isAdmin = (currentUser == adminRole) ? console.log("Привет, Admin"): console.log("Привет, Anonymus"); 
+let isAdmin = (currentUser == adminRole) ? console.log("Привет, Admin"): console.log("Привет, Anonymus");
+function sum (a, b) {
+    alert (a+b);
+}
+sum(1 , 2);
+
+// function log() {
+//     for (var i = 0; i < arguments.length; i++)
+// }
+
+let sum = (a, b) => a + b;
+
+function min (a, b) {
+    if (a < b) {
+      return a
+    }
+    else {
+      return b
+    }
+ }
+console.log(min(5, 4))
+
+
+function range (Start, max, number) {
+    if (number < Start && number > max ){
+      alert ("true")
+    }
+   else {
+    alert ("false")
+  }
+  }
+
+  const user = {
+    name: "John",
+    surname: "Smith"
+  }
+    user.name = 'Pete';
+    delete user.name
+
+    console.log(user)
+
+    let salaries = {
+        John: 100,
+        Ann: 160,
+        Pete: 130
+    }
+
+    function countTotalSalaries(salaries) {
+        let res = 0;
+        for ( let key in salaries) {
+            res += salaries[key]
+        }
+        return res;
+    }
+    countTotalSalaries(salaries)
