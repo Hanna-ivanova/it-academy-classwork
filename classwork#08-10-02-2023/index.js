831852e29f7e2dcc99aa9632456e987bae5fe99d
Hanna Ivanova <hanna.ivanovadl@gmail.com>
2023-02-14 17:27:31 +0300

    commit lesson 8

---
+++ classwork#08-10-02-2023/index.js
@@ -0,0 +1,12 @@
+const table = document.getElementById('age-table');
+
+const label = document.getElementsByTagName('label');
+
+const td = document.querySelector('.td');
+
+const formSearch = document.getElementsByName('search');
+
+const firstInput = form.querySelector('.input:first-child');
+const lastInput = form.querySelector('.input:last-child');
+
+console.log()
